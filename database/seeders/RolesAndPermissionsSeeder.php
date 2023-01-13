7d8f96bd9603b0f9a0fc48393736a09c6a82473c
davydocsurg <davydocsurg@gmail.com>
2023-01-13 16:18:14 +0100

    add roles and permissions and user seeder

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        // Permission::create(['name' => 'edit articles']);
+
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'Patients']);
+        Role::create(['name' => 'Providers']);
+    }
+}
